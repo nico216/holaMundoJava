a1768cc68009a45c26bed10466c36690f0908466
Nicolas Briones <Nicolas Briones>
2016-01-10 16:09:30 -0300

    modificado

--- src/paquete/HolaMundo.java
+++ src/paquete/HolaMundo.java
@@ -4,8 +4,8 @@ class HolaMundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.xvutdxvgcxv xdv.println("Hola Mundo");
-		System.out.println("vxcHola Git");
+		System.out.println("Hola Nico");
+		
 
 	}
 
